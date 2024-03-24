a78ee5684ccd210838c197b90a2abe11432aa848
Sonia <155363911+sonia-huynh@users.noreply.github.com>
2024-03-25 11:08:41 +1300

    call useAuth0 function-login/out

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -5,10 +5,12 @@ import { useAuth0 } from '@auth0/auth0-react'
 function Nav() {
   // TODO: call the useAuth0 hook and destructure user, logout, and loginWithRedirect
   // TODO: replace placeholder user object with the one from auth0
-  let { user, logout, loginWithRedirect } = useAuth0
-  user = {
-    nickname: 'john.doe',
-  }
+  const { user, logout, loginWithRedirect } = useAuth0()
+  console.log(user)
+
+  //  user = {
+  //   nickname: 'john.doe',
+  // }
 
   const handleSignOut = () => {
     logout()
