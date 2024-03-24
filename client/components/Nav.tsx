0a43c86b5464ae593e383ce4602da7293e5ec3ba
Sonia <155363911+sonia-huynh@users.noreply.github.com>
2024-03-25 10:47:25 +1300

    allow user to log in/out using Auth0

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,18 +1,22 @@
 import { IfAuthenticated, IfNotAuthenticated } from './Authenticated.tsx'
 import { NavGroup, NavButton } from './Styled.tsx'
+import { useAuth0 } from '@auth0/auth0-react'
 
 function Nav() {
   // TODO: call the useAuth0 hook and destructure user, logout, and loginWithRedirect
   // TODO: replace placeholder user object with the one from auth0
-  const user = {
+  let { user, logout, loginWithRedirect } = useAuth0
+  user = {
     nickname: 'john.doe',
   }
 
   const handleSignOut = () => {
+    logout()
     console.log('sign out')
   }
 
   const handleSignIn = () => {
+    loginWithRedirect()
     console.log('sign in')
   }
 
